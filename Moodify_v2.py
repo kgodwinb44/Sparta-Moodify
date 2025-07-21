c64d6a969ae2870499ea995e225737be5a28b224
Hunzaa <nooramin261126@gmail.com>
2025-07-21 09:40:15 +0100

    Created Moodify v2 that works with Deezer

--- Moodify_v2.py
+++ Moodify_v2.py
@@ -1,1 +1,25 @@
 # App for Deezer API
+import requests
+
+# By artist name
+artist_name = "sab"
+resultByArtist = requests.get("https://api.deezer.com/search", params={"q": artist_name})
+song = resultByArtist.json()["data"][0]
+print("\nSong by Artist")
+print("Artist:", song["artist"]["name"])
+print("Song:", song["title"])
+print("Link:", song["link"])
+
+
+# By mood and year (figure out year part)
+# Set mood and year
+mood = "happy"
+year = "2025"
+resultByMood = requests.get("https://api.deezer.com/search", params={"q": mood, "limit": 1})
+song = resultByMood.json()["data"][0]
+
+print("\n🎵 Song by Mood and Year")
+print("Song:", song["title"])
+print("Artist:", song["artist"]["name"])
+print("Album:", song["album"]["title"])
+print("Link:", song["link"])
